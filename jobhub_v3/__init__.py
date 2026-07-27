ed64c74b2942d52589b09713e89d13c9f0a155ef
BrycePremierBW <bryce@premierbw.com.au>
2026-07-27 21:58:02 +1000

    Complete V4 stack jobhub_v3/__init__.py

--- jobhub_v3/__init__.py
+++ jobhub_v3/__init__.py
@@ -5,13 +5,24 @@ from .mappings import (
     build_purchase_bill_payload,
     build_sales_invoice_payload,
 )
+from .commercial import (
+    calculate_eot_due_date_extension,
+    calculate_progress_claim,
+    validate_transition,
+)
+from .sync_service import XeroSyncEventStore, XeroSyncService
 from .xero_client import XeroClient, XeroOAuthConfig, XeroToken
 
 __all__ = [
     "XeroClient",
     "XeroOAuthConfig",
+    "XeroSyncEventStore",
+    "XeroSyncService",
     "XeroToken",
     "build_contact_payload",
     "build_purchase_bill_payload",
     "build_sales_invoice_payload",
+    "calculate_eot_due_date_extension",
+    "calculate_progress_claim",
+    "validate_transition",
 ]
