f17f9a4aeac5c8ffdd4207e55f4acd7cefd7521c
BrycePremierBW <bryce@premierbw.com.au>
2026-07-27 21:57:02 +1000

    Complete staged roadmap service jobhub_v2/__init__.py

--- jobhub_v2/__init__.py
+++ jobhub_v2/__init__.py
@@ -1,11 +1,17 @@
 """Premier Brushworks JobHub V2 service layer."""
 
 from .config import RuntimeConfig, load_runtime_config
+from .email_delivery import CriticalEmailOutbox
 from .idempotency import build_idempotency_key, normalise_sync_payload
+from .schema import ensure_v2_schema
+from .sync import OfflineSyncProcessor
 
 __all__ = [
+    "CriticalEmailOutbox",
+    "OfflineSyncProcessor",
     "RuntimeConfig",
     "build_idempotency_key",
+    "ensure_v2_schema",
     "load_runtime_config",
     "normalise_sync_payload",
 ]
