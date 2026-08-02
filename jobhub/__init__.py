81143c561ac7465aaf3c2148c07c1e3ceba1b303
BrycePremierBW <bryce@premierbw.com.au>
2026-08-03 09:20:15 +1000

    Install stage preset guard

--- jobhub/__init__.py
+++ jobhub/__init__.py
@@ -4,6 +4,7 @@ from .mobile_sidebar_guard import install_mobile_sidebar_guard
 from .navigation_state_guard import install_navigation_state_guard
 from .notification_wording_guard import install_notification_wording_guard
 from .push_configuration_guard import install_push_configuration_guard
+from .stage_preset_guard import install_stage_preset_guard
 from .stage_selection_guard import install_stage_selection_guard
 
 # Install before the main app calls st.set_page_config/apply_pb_branding.  These
@@ -14,3 +15,4 @@ install_notification_wording_guard()
 install_mobile_sidebar_guard()
 install_navigation_state_guard()
 install_stage_selection_guard()
+install_stage_preset_guard()
