207fc1db04eaf95c5938d0da2e03ad99ed372b24
BrycePremierBW <bryce@premierbw.com.au>
2026-08-03 16:13:31 +1000

    Install progress baseline unlock panel

--- jobhub/__init__.py
+++ jobhub/__init__.py
@@ -9,6 +9,7 @@ from .notification_wording_guard import install_notification_wording_guard
 from .po_upload_guard import install_po_upload_guard
 from .po_upload_scope_return_guard import install_po_upload_scope_return_guard
 from .po_upload_split_guard import install_po_upload_split_guard
+from .progress_baseline_unlock_guard import install_progress_baseline_unlock_guard
 from .progress_external_options_guard import install_progress_external_options_guard
 from .push_configuration_guard import install_push_configuration_guard
 from .sidebar_readability_guard import install_sidebar_readability_guard
@@ -32,6 +33,7 @@ install_navigation_state_guard()
 install_po_upload_guard()
 install_po_upload_scope_return_guard()
 install_po_upload_split_guard()
+install_progress_baseline_unlock_guard()
 install_job_folder_uploaded_documents_guard()
 install_progress_external_options_guard()
 install_stage_selection_guard()
