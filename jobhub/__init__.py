de30287d7c7c706f6cb6227dfe61fb5efe9fce0e
BrycePremierBW <bryce@premierbw.com.au>
2026-08-03 21:26:26 +1000

    Wire setup defaults and dwelling stage builder

--- jobhub/__init__.py
+++ jobhub/__init__.py
@@ -12,7 +12,9 @@ from .po_upload_split_guard import install_po_upload_split_guard
 from .progress_baseline_unlock_guard import install_progress_baseline_unlock_guard
 from .progress_external_options_guard import install_progress_external_options_guard
 from .push_configuration_guard import install_push_configuration_guard
+from .setup_defaults_guard import install_setup_defaults_guard
 from .sidebar_readability_guard import install_sidebar_readability_guard
+from .stage_dwelling_builder_guard import install_stage_dwelling_builder_guard
 from .stage_preset_guard import install_stage_preset_guard
 from .stage_preset_selector_fix_guard import install_stage_preset_selector_fix_guard
 from .stage_preset_visibility_guard import install_stage_preset_visibility_guard
@@ -31,6 +33,7 @@ install_notification_wording_guard()
 install_mobile_sidebar_guard()
 install_sidebar_readability_guard()
 install_navigation_state_guard()
+install_setup_defaults_guard()
 install_po_upload_guard()
 install_po_upload_scope_return_guard()
 install_po_upload_split_guard()
@@ -39,6 +42,7 @@ install_job_folder_uploaded_documents_guard()
 install_progress_external_options_guard()
 install_stage_selection_guard()
 install_stage_preset_selector_fix_guard()
+install_stage_dwelling_builder_guard()
 install_stage_preset_guard()
 install_stage_preset_visibility_guard()
 install_timesheet_area_guard()
