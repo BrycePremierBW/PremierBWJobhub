8023f8cd2f7dd0027321a7096dbcdee3d5d7daf4
BrycePremierBW <bryce@premierbw.com.au>
2026-08-05 01:15:48 +1000

    Add lean page exports

---
+++ jobhub_lean/pages.py
@@ -0,0 +1,17 @@
+"""Page exports for the lean JobHub shell."""
+
+from .common import AppContext
+from .directory import builders_page, dashboard_page, employees_page, products_page
+from .jobs import jobs_page, purchase_orders_page, stages_page
+from .timesheets import timesheets_page
+from .resources import equipment_page, materials_page
+from .estimating import estimating_page, recalc_estimate
+from .records import job_files_page, reports_page
+from .admin import external_page, system_page, users_page
+
+__all__ = [
+    "AppContext", "builders_page", "dashboard_page", "employees_page",
+    "equipment_page", "estimating_page", "external_page", "job_files_page",
+    "jobs_page", "materials_page", "products_page", "reports_page",
+    "system_page", "timesheets_page", "users_page",
+]
