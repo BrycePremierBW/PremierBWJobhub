b2423c31f45750d92234c5d95d46472c005e5838
BrycePremierBW <bryce@premierbw.com.au>
2026-08-05 01:59:22 +1000

    Expose the dedicated Upload PO page

--- jobhub_lean/pages.py
+++ jobhub_lean/pages.py
@@ -7,6 +7,7 @@ from .timesheets import timesheets_page
 from .resources import equipment_page, materials_page
 from .estimating import estimating_page, recalc_estimate
 from .job_packs import job_pack_import_page
+from .po_upload import po_upload_page
 from .records import job_files_page, reports_page
 from .setup import setup_page
 from .admin import external_page, system_page, users_page
@@ -14,6 +15,7 @@ from .admin import external_page, system_page, users_page
 __all__ = [
     "AppContext", "builders_page", "dashboard_page", "employees_page",
     "equipment_page", "estimating_page", "external_page", "job_files_page",
-    "job_pack_import_page", "jobs_page", "materials_page", "products_page",
-    "reports_page", "setup_page", "system_page", "timesheets_page", "users_page",
+    "job_pack_import_page", "jobs_page", "materials_page", "po_upload_page",
+    "products_page", "reports_page", "setup_page", "system_page",
+    "timesheets_page", "users_page",
 ]
