028d3a1dba7e9ce03ff56142cb5368f54e352891
BrycePremierBW <bryce@premierbw.com.au>
2026-08-05 01:49:56 +1000

    Expose the modular Job Pack page

--- jobhub_lean/pages.py
+++ jobhub_lean/pages.py
@@ -6,12 +6,13 @@ from .jobs import jobs_page, purchase_orders_page, stages_page
 from .timesheets import timesheets_page
 from .resources import equipment_page, materials_page
 from .estimating import estimating_page, recalc_estimate
+from .job_packs import job_pack_import_page
 from .records import job_files_page, reports_page
 from .admin import external_page, system_page, users_page
 
 __all__ = [
     "AppContext", "builders_page", "dashboard_page", "employees_page",
     "equipment_page", "estimating_page", "external_page", "job_files_page",
-    "jobs_page", "materials_page", "products_page", "reports_page",
-    "system_page", "timesheets_page", "users_page",
+    "job_pack_import_page", "jobs_page", "materials_page", "products_page",
+    "reports_page", "system_page", "timesheets_page", "users_page",
 ]
