640a526647fe702d82b4102d300bb4af88b21135
BrycePremierBW <bryce@premierbw.com.au>
2026-08-21 19:43:35 +1000

    Enable stay-signed-in support at JobHub startup

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+from jobhub.persistent_login import install as install_persistent_login
+
+install_persistent_login()
+
 import pb_jobhub_app
