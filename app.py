36820f0224469b07f4dcbcfcca3a5024dfac35a0
BrycePremierBW <bryce@premierbw.com.au>
2026-08-24 16:36:21 +1000

    Install bulk timesheet job reassignment at startup

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import sys
 sys.path.insert(0, os.path.dirname(__file__))
 
 from jobhub.persistent_login import install as install_persistent_login
+from jobhub.timesheet_bulk_reassign import install as install_timesheet_bulk_reassign
 
 install_persistent_login()
+install_timesheet_bulk_reassign()
 
 import pb_jobhub_app
